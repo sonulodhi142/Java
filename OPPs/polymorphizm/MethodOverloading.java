c267e3d3a0391a819d860977b60f1de17b40bb46
Sonu Lodhi <sp95759201@gmail.com>
2025-05-12 13:43:35 +0530

    code

--- OPPs/polymorphizm/MethodOverloading.java
+++ OPPs/polymorphizm/MethodOverloading.java
@@ -1,5 +1,16 @@
 package polymorphizm;
-
+class Operation{
+    int sum(int a, int b){
+        return a+b;
+    }
+    double sum(double a,double b){
+        return a+b;
+    }
+}
 public class MethodOverloading {
-    
+    public static void main(String[] args) {
+        Operation obj = new Operation();
+        System.out.println(obj.sum(6,7));
+        System.out.println(obj.sum(5.5, 5.5));
+    }
 }
